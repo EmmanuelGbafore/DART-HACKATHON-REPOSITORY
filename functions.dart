b62a9888152c031ac1cc082c6d723599d072d67c
Emmanuel Gbafore <egbafore@gmail.com>
2024-03-19 09:50:34 +0300

    My Dart-Hackathon-Code

--- functions.dart
+++ functions.dart
@@ -1,2 +1,26 @@
-## Program 2: Perform Mathematical Operations with Functions
-Write a Dart program that performs two mathematical operations using functions.
+//## Program 2: Perform Mathematical Operations with Functions
+//Write a Dart program that performs two mathematical operations using functions.
+
+// First Mathematical Operation: Area of a Rectangle
+int areaOfRectangle( int length,int width){
+  int area = length * width;
+  return area;
+}
+// Second Mathematical Operation: Perimeter of a Rectangle
+int perimeterOfRectangle(int length, int width){
+  int perimeter = 2*(length+width);
+  return perimeter;
+}
+
+void main(){
+  int length = 5;
+  int width = 6;
+
+  // Functions call
+  int area = areaOfRectangle(length,width);
+  int perimeter = perimeterOfRectangle(length,width);
+  
+  // Printing of outputs
+  print("The area of the rectangle is $area.");
+  print("The perimeter of the rectangle is $perimeter");
+}
--- program1..dart
+++ program1..dart
@@ -1,2 +1,12 @@
-## Program 1: Display Personal Information
-Write a Dart program using variables to display your name, age, school, and a hobby. Print it in one logical sentence.
+//## Program 1: Display Personal Information
+//Write a Dart program using variables to display your name, age, school, and a hobby. Print it in one logical sentence.
+
+void main (){
+  String name = "Emmanuel Gbafore";   // My name
+  int age = 31;    // My age
+  String school = "Power Learn Project Academy";    // My school
+  String hobby = "reading";   // My hobby
+
+  // Command to print out in a sentence
+  print("I am $name, a $age years old student at $school and $hobby is my favorite passtime." );
+}
